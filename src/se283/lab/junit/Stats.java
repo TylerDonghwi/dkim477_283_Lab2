6dc72a3ce084dd4fd784e79cea5b24514511e191
TylerDonghwi <tylerdonghwikim@gmail.com>
2022-07-29 00:34:24 +1200

    hello world

--- src/se283/lab/junit/Stats.java
+++ src/se283/lab/junit/Stats.java
@@ -8,62 +8,62 @@ package se283.lab.junit;
 
 public class Stats {
 
-    private int[] _numbers;
+	private int[] _numbers;
 
-    /**
-     * Create a Stats object with a set of integers
-     * 
-     * @param intNumbers An array of integers, on which means or standard
-     *                   deviations could be computed.
-     */
-    public Stats(int[] intNumbers) {
-        _numbers = intNumbers;
-    }
+	/**
+	 * Create a Stats object with a set of integers
+	 * 
+	 * @param intNumbers An array of integers, on which means or standard deviations
+	 *                   could be computed.
+	 */
+	public Stats(int[] intNumbers) {
+		_numbers = intNumbers;
+	}
 
-    /**
-     * A private method to calculate mean of ints stored in numbers and return
-     * the value as a double
-     * 
-     * @return The mean of the list of numbers that the Stats object maintains.
-     */
-    private double mean() {
+	/**
+	 * A private method to calculate mean of ints stored in numbers and return the
+	 * value as a double
+	 * 
+	 * @return The mean of the list of numbers that the Stats object maintains.
+	 */
+	private double mean() {
 
-        if (_numbers.length == 0) {
-            throw new RuntimeException("Empty input");
-        }
-        double sum = 0;
+		if (_numbers.length == 0) {
+			throw new RuntimeException("Empty input");
+		}
+		double sum = 0;
 
-        for (int i = 0; i < _numbers.length; i++) {
-            if (_numbers[i] < 0) {
-                throw new RuntimeException("Input should be non-negative integer");
-            }
-            sum = sum + _numbers[i];
-        }
+		for (int i = 0; i < _numbers.length; i++) {
+			if (_numbers[i] < 0) {
+				throw new RuntimeException("Input should be non-negative integer");
+			}
+			sum = sum + _numbers[i];
+		}
+		System.out.println("HELLO WORLD");
+		return sum / _numbers.length;
+	}
 
-        return sum / _numbers.length;
-    }
+	/**
+	 * Return the rounded int value of mean
+	 */
+	public int getMean() {
+		return (int) (mean() + 0.5);
+	}
 
-    /**
-     * Return the rounded int value of mean
-     */
-    public int getMean() {
-        return (int) (mean() + 0.5);
-    }
+	/**
+	 * Calculate standard deviation of ints stored in numbers and return the rounded
+	 * int value
+	 * 
+	 * @return The mean of the list of numbers that the Stats object maintains.
+	 */
+	public int getStdDevn() {
+		double mean = getMean();
+		double standardDeviation = 0;
 
-    /**
-     * Calculate standard deviation of ints stored in numbers and return the
-     * rounded int value
-     * 
-     * @return The mean of the list of numbers that the Stats object maintains.
-     */
-    public int getStdDevn() {
-        double mean = getMean();
-        double standardDeviation = 0;
+		for (int num : _numbers) {
+			standardDeviation += Math.pow(num - mean, 2);
+		}
 
-        for (int num : _numbers) {
-            standardDeviation += Math.pow(num - mean, 2);
-        }
-
-        return (int) Math.sqrt(standardDeviation / _numbers.length);
-    }
+		return (int) Math.sqrt(standardDeviation / _numbers.length);
+	}
 }
